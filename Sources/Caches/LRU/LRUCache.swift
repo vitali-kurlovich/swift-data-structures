7278e93bbb556934887b18c4e142c4766ffac59e
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-03-06 22:37:36 +0400

    Add LRUCache

---
+++ Sources/Caches/LRU/LRUCache.swift
@@ -0,0 +1,95 @@
+//
+//  Created by Vitali Kurlovich on 6.03.26.
+//
+
+import Lists
+
+public final class LRUCache<Key: Hashable, T> {
+    public let maxCacheSize: Int
+
+    public init(maxCacheSize: Int = 64) {
+        assert(maxCacheSize > 0)
+        self.maxCacheSize = maxCacheSize
+    }
+
+    let storage = LinkedList<KeyValue>()
+    var map: [Key: MapValue] = [:]
+}
+
+extension LRUCache {
+    typealias MapValue = ListNode<KeyValue>
+
+    struct KeyValue {
+        let key: Key
+        let value: T
+    }
+}
+
+public extension LRUCache {
+    var count: Int {
+        map.count
+    }
+
+    func contains(_ key: Key) -> Bool {
+        map[key] != nil
+    }
+}
+
+public extension LRUCache {
+    subscript(_ key: Key) -> T? {
+        get {
+            pull(key: key)
+        }
+        set {
+            if let newValue {
+                push(key: key, value: newValue)
+            } else {
+                remove(by: key)
+            }
+        }
+    }
+}
+
+public extension LRUCache {
+    func pull(key: Key) -> T? {
+        guard let node = map[key] else {
+            return nil
+        }
+
+        storage.remove(node)
+        storage.prepend(node: node)
+
+        return node.value.value
+    }
+
+    func push(key: Key, value: T) {
+        let stored = KeyValue(key: key, value: value)
+
+        if map.count < maxCacheSize {
+            let node = storage.prepend(stored)
+            map[key] = node
+        } else {
+            let node = storage.dropLast()!
+            map[node.value.key] = nil
+            node.value = stored
+            map[key] = node
+            storage.prepend(node: node)
+        }
+    }
+}
+
+public extension LRUCache {
+    func removeAll() {
+        storage.removeAll()
+        map.removeAll()
+    }
+
+    func remove(by key: Key) {
+        guard let node = map[key] else {
+            return
+        }
+
+        storage.remove(node)
+        map[key] = nil
+    }
+}
---
+++ Tests/CachesTests/LRUCache/LRUCacheTests.swift
@@ -0,0 +1,61 @@
+//
+//  Created by Vitali Kurlovich on 6.03.26.
+//
+
+@testable import Caches
+@testable import Lists
+import Testing
+
+@Suite("LRUCache")
+struct LRUCacheTests {}
+
+extension LRUCacheTests {
+    @Test("Push")
+    func push() {
+        let cache = LRUCache<Int, String>(maxCacheSize: 4)
+
+        cache[0] = "0"
+        #expect(cache.count == 1)
+        #expect(cache[0] == "0")
+        #expect(cache[1] == nil)
+
+        cache[1] = "1"
+        #expect(cache.count == 2)
+        #expect(cache[0] == "0")
+        #expect(cache[1] == "1")
+        #expect(cache[2] == nil)
+
+        cache[2] = "2"
+        #expect(cache.count == 3)
+        #expect(cache[0] == "0")
+        #expect(cache[1] == "1")
+        #expect(cache[2] == "2")
+        #expect(cache[3] == nil)
+
+        cache[3] = "3"
+        #expect(cache.count == 4)
+        #expect(cache[0] == "0")
+        #expect(cache[1] == "1")
+        #expect(cache[2] == "2")
+        #expect(cache[3] == "3")
+
+        cache[4] = "4"
+        #expect(cache.count == 4)
+        #expect(cache[0] == nil)
+        #expect(cache[1] == "1")
+        #expect(cache[2] == "2")
+        #expect(cache[3] == "3")
+        #expect(cache[4] == "4")
+
+        #expect(cache[1] == "1")
+        cache[5] = "5"
+
+        #expect(cache.count == 4)
+        #expect(cache[0] == nil)
+        #expect(cache[1] == "1")
+        #expect(cache[2] == nil)
+        #expect(cache[3] == "3")
+        #expect(cache[4] == "4")
+        #expect(cache[5] == "5")
+    }
+}
