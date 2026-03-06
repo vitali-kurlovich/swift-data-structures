b0925418dc012cd9211ef00df17625bf7f1b0ce0
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-03-06 23:53:39 +0400

    implements Sequence protocol for ListNode

---
+++ Sources/Lists/LinkedList/Node/ListNode+Sequence.swift
@@ -0,0 +1,28 @@
+//
+//  Created by Vitali Kurlovich on 6.03.26.
+//
+
+extension ListNode: Sequence {
+    public typealias Iterator = ListNodeIterator<T>
+
+    public func makeIterator() -> ListNodeIterator<T> {
+        Iterator(self)
+    }
+}
+
+public struct ListNodeIterator<T>: IteratorProtocol {
+    public typealias Element = T
+
+    var node: ListNode<T>?
+
+    public init(_ node: ListNode<T>) {
+        self.node = node
+    }
+
+    public mutating func next() -> T? {
+        defer {
+            node = node?.next
+        }
+        return node?.value
+    }
+}
